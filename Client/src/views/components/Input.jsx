37714d43ff61e9c25db888d601d0c52fe3d6c8fd
Djoufson <djouflegran@gmail.com>
2022-12-18 09:51:58 -0800

    Input component 100%

--- Client/src/views/components/Input.jsx
+++ Client/src/views/components/Input.jsx
@@ -2,26 +2,38 @@ import { useState } from "react";
 
 const Input = ({ id, placeholder, setValue, type }) => {
   const [className, setClassName] = useState('input-wrapper')
-  const entry = document.getElementById(id)
   const handleClick = (e) => {
+    const entry = document.getElementById(id)
+    entry.focus()
     if (className === 'input-wrapper focus'){
       return
     }
     //console.log(entry)
-    entry.focus()
     setClassName("input-wrapper focus")
   }
 
+  const handleChange = (e) => {
+    const value = e.target.value
+    if(value === ""){
+      setClassName("input-wrapper")
+      return
+    }
+
+    setClassName("input-wrapper focus")
+    setValue(e.target.value)
+  }
   const handleBlur = (e) => {
-    setClassName("input-wrapper")
+    const entry = document.getElementById(id)
+    if(entry.value === "")
+      setClassName("input-wrapper")
   }
 
   return ( 
     <>
       <div className={ className }>
         <div className="click-handler" onClick={ handleClick }></div>
-        <label htmlFor={ id } onBlur={ handleBlur } className="placeholder">{ placeholder }</label>
-        <input id={ id } type={ type } onChange={(e) => (setValue(e.target.value))} />
+        <label htmlFor={ id } className="placeholder">{ placeholder }</label>
+        <input onBlur={ handleBlur } id={ id } type={ type } onChange={ (e) => handleChange(e) } />
       </div>
     </>
   );
